dfc44934a2c213b1a9b36468b97a1fc103f1ecc9
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-06-04 15:16:55 +0500

    successful firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'configs/app_colors.dart';
 import 'configs/app_font.dart';
 import 'pages/main_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
