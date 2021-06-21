71e7133a675353c0885ee757406084d27e4559a3
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-06-21 23:19:41 +0500

    improve recent project

--- lib/pages/recent_projects/project_card.dart
+++ lib/pages/recent_projects/project_card.dart
@@ -10,41 +10,74 @@ class ProjectCard extends StatefulWidget {
 }
 
 class _ProjectCardState extends State<ProjectCard> {
+  bool isHover = false;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Stack(
-          children: [
-            Container(
-              height: 220,
-              width: 260,
-              child: Image(
-                image: AssetImage(widget.projects.thumnail),
-                fit: BoxFit.fill,
+    return InkWell(
+      onTap: () {},
+      onHover: (value) {
+        setState(() {
+          isHover = value;
+        });
+      },
+      child: (isHover)
+          ? ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Container(
+                height: 220,
+                width: 260,
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  widget.projects.description,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 7,
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white70,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+            )
+          : Container(
+              margin: const EdgeInsets.all(10),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 220,
+                      width: 260,
+                      child: Image(
+                        image: AssetImage(widget.projects.thumnail),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Container(
+                      height: 220,
+                      width: 260,
+                      color: Colors.black.withOpacity(0.3),
+                    ),
+                    Container(
+                      height: 220,
+                      width: 260,
+                      padding: const EdgeInsets.all(8),
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        widget.projects.title,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            Container(
-              height: 220,
-              width: 260,
-              color: Colors.black.withOpacity(0.3),
-            ),
-            Container(
-              height: 220,
-              width: 260,
-              padding: const EdgeInsets.all(8),
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                widget.projects.title,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
--- lib/pages/recent_projects/recent_projects_page.dart
+++ lib/pages/recent_projects/recent_projects_page.dart
@@ -15,31 +15,31 @@ class _RecentProjectsPageState extends State<RecentProjectsPage> {
     RecentProjects(
       title: 'SafarKar - Travel & Explore PK',
       description:
-          'Users can make trip plans and can also view others plan\nLanguage: Flutter, Dart\nDatabase: Firebase\nGoogle direction, places, map API\n',
+          'Users can make trip plans and can also view others plan\n\nLanguage: Flutter, Dart\nDatabase: Firebase\nGoogle direction, places, map API\n',
       thumnail: iSafarKarLogo,
     ),
     RecentProjects(
       title: 'Review App',
       description:
-          'Users can add product and other users can add/view video review about that product.\nLanguage: Flutter, Dart\nDatabase: Firebase',
+          'Users can add product and other users can add/view video review about that product.\n\nLanguage: Flutter, Dart\nDatabase: Firebase',
       thumnail: iReviewLogo,
     ),
     RecentProjects(
       title: 'NorthWest Reverse Mortgage App',
       description:
-          'Users can calculate the mortgage value.\nLanguage: Flutter, Dart\nDatabase: Firebase',
+          'Users can calculate the mortgage value.\n\nLanguage: Flutter, Dart\nDatabase: Firebase',
       thumnail: iNorthwestLogo,
     ),
     RecentProjects(
       title: 'Golden Time Treading App',
       description:
-          'Users can Tread realtime. Users can also chat with others.\nLanguage: Flutter, Dart\nDatabase: Firebase',
+          'Users can Tread realtime. Users can also chat with others.\n\nLanguage: Flutter, Dart\nDatabase: Firebase',
       thumnail: iGoldenTimeTreadingLogo,
     ),
     RecentProjects(
       title: 'Easyio App',
       description:
-          'Online POS for any shop. Users can generate bill search item and details.\nLanguage: Flutter, Dart\nDatabase: Firebase',
+          'Online POS for any shop. Users can generate bill search item and details.\n\nLanguage: Flutter, Dart\nDatabase: Firebase',
       thumnail: iEasyioLogo,
     ),
   ];
@@ -51,21 +51,33 @@ class _RecentProjectsPageState extends State<RecentProjectsPage> {
         ? Container()
         : Container(
             padding: (size.width > 800)
-                ? const EdgeInsets.symmetric(horizontal: 120)
-                : const EdgeInsets.symmetric(horizontal: 40),
+                ? const EdgeInsets.all(120)
+                : const EdgeInsets.all(40),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 CustomizeUnderlineText(size: size, title: 'Recent Projects'),
                 SizedBox(
-                  height: 300,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: _recentProjects.length,
-                    itemBuilder: (context, index) {
-                      return ProjectCard(projects: _recentProjects[index]);
-                    },
+                  height: 230,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: _recentProjects.length,
+                          itemBuilder: (context, index) {
+                            return ProjectCard(
+                                projects: _recentProjects[index]);
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        color: Theme.of(context).primaryColor.withOpacity(0.2),
+                      ),
+                    ],
                   ),
                 ),
               ],
