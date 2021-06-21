e415df72b52ad9a598c7177e1387487bacd1bf42
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-06-22 00:02:30 +0500

    add scrolling buttons

--- lib/pages/main_screen.dart
+++ lib/pages/main_screen.dart
@@ -1,27 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/animation/entrance_fader.dart';
 import 'package:portfolio/pages/about/about.dart';
 import 'package:portfolio/pages/contact/contact.dart';
 import 'package:portfolio/pages/home/home.dart';
 import 'package:portfolio/pages/recent_projects/recent_projects_page.dart';
 import 'package:portfolio/pages/services/services.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'dart:html' as html;
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  ScrollController _scrollController =
+      ScrollController(initialScrollOffset: 25.0);
+  ItemScrollController _itemScrollController = ItemScrollController();
+  ItemPositionsListener _itemPositionListener = ItemPositionsListener.create();
+
+  final List<String> _sectionsName = [
+    "Home",
+    "About",
+    "Services",
+    "Projects",
+    "Contact"
+  ];
+
+  final List<IconData> _sectionsIcons = [
+    Icons.home,
+    Icons.person,
+    Icons.settings,
+    Icons.build,
+    Icons.phone,
+  ];
+
+  void _scroll(int i) {
+    _itemScrollController.scrollTo(index: i, duration: Duration(seconds: 1));
+  }
+
+  Widget sectionWidget(int i) {
+    if (i == 0) {
+      return HomePage();
+    } else if (i == 1) {
+      return AboutPage();
+    } else if (i == 2) {
+      return ServicePage();
+    } else if (i == 3) {
+      return RecentProjectsPage();
+    } else if (i == 4) {
+      return ContactPage();
+    } else if (i == 5) {
+      return SizedBox(
+        height: 40.0,
+      );
+    } else if (i == 6) {
+      return ContactPage();
+    } else {
+      return Container();
+    }
+  }
+
+  Widget _appBarActions(String childText, int index, IconData icon) {
+    return MediaQuery.of(context).size.width > 760
+        ? EntranceFader(
+            offset: Offset(0, -20),
+            delay: Duration(seconds: 3),
+            duration: Duration(seconds: 1),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                hoverColor: Theme.of(context).primaryColor,
+                onPressed: () => _scroll(index),
+                child: Text(
+                  childText,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          )
+        : Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              tooltip: '$childText',
+              padding: const EdgeInsets.all(10),
+              splashRadius: 20,
+              hoverColor: Theme.of(context).primaryColor,
+              onPressed: () => _scroll(index),
+              icon: Icon(icon, color: Colors.white70),
+            ),
+          );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            HomePage(),
-            AboutPage(),
-            ServicePage(),
-            RecentProjectsPage(),
-            ContactPage(),
-          ],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          for (int i = 0; i < _sectionsName.length; i++)
+            _appBarActions(_sectionsName[i], i, _sectionsIcons[i]),
+          EntranceFader(
+            offset: Offset(0, -20),
+            delay: Duration(seconds: 3),
+            duration: Duration(seconds: 1),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                hoverColor: Theme.of(context).primaryColor.withAlpha(150),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    side: BorderSide(color: Theme.of(context).primaryColor)),
+                onPressed: () {
+                  html.window.open(
+                      'https://drive.google.com/file/d/1abY079fdgZ2VkNOAppX3KyY_wr3MuaGu/view?usp=sharing',
+                      "pdf");
+                },
+                child: Text(
+                  "Resume",
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: RawScrollbar(
+          controller: _scrollController,
+          // thumbColor: kPrimaryColor,
+          thickness: 5.0,
+          child: ScrollablePositionedList.builder(
+            itemScrollController: _itemScrollController,
+            itemPositionsListener: _itemPositionListener,
+            itemCount: 8,
+            itemBuilder: (context, index) {
+              return sectionWidget(index);
+            },
+          ),
         ),
       ),
     );
